7de4038ddde26a67caf4e9d2faaf455b857f18f5
tmxk4 <tmxk4197@naver.com>
2024-04-09 12:06:09 +0900

    add

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd
+          Learn GithubAction1
         </a>
       </header>
     </div>
