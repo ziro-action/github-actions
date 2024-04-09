e10ff5c6234466346108c09d94d1d96c22cc625f
CMC <tmxk4197@naver.com>
2024-04-09 23:13:03 +0900

    Update App.js (#16)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd1 test
+          Learn GithubAction cicd1 test 1
         </a>
       </header>
     </div>
