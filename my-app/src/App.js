56f88b3eb41c70cdf7c7bd9706c444febe65c5b7
tmxk4 <tmxk4197@naver.com>
2024-04-09 12:08:38 +0900

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction1
+          Learn GithubAction2
         </a>
       </header>
     </div>
